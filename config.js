2580ec255bc627cece9d3fbbb75a782b1fbe1801
Nammmva <91364062+Nammmva@users.noreply.github.com>
2021-09-25 14:49:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Hế lu Công chúa',
+    introDesc: `Ny biết gì không ??`,
+    btnIntro: 'Muốn biết thì nhấn vào đây nhé',
+    title: 'Bé có yêu tới không nhờ ?',
+    desc: 'Tắt là dỗi suốt đời nhó.....',
+    btnYes: 'Yêu chứ :)))',
+    btnNo: 'CÓ cái nịt nhó',
+    question:'Thế sao yêu tới nhờ',
+    btnReply: 'SUBMIT',
+    reply: 'Vì Văn Nam siêu đẹp trai cấp vũ trụ, ngầu lòi nên mình yêu đó',
+    mess: 'Dù mọi Ny có ghi lý do gì thì mình vẫn luôn yêu bé nhó my baby',
+    messDesc: 'Bé muốn biết biết thêm không ???',
+    btnAccept: 'Muốnnnnnnnnnn',
+    messLink: 'file:///D:/Desktop/pp%20t%C3%ACm%20ki%E1%BA%BFm/ngh-c-master/ngh-c-master/dd.html',
+    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
